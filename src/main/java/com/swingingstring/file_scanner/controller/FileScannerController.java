e0990c41d17a927ed59e1ed6cb74d0cc55c302fd
lehel.tanko <lehel.tanko@erstebank.hu>
2025-10-09 19:55:12 +0200

    optimized imports

--- src/main/java/com/swingingstring/file_scanner/controller/FileScannerController.java
+++ src/main/java/com/swingingstring/file_scanner/controller/FileScannerController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
 import java.util.List;
 
 @RestController
--- src/main/java/com/swingingstring/file_scanner/service/HistoryService.java
+++ src/main/java/com/swingingstring/file_scanner/service/HistoryService.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.swingingstring.file_scanner.entity.HistoryItem;
 import com.swingingstring.file_scanner.model.FileItem;
-
 import com.swingingstring.file_scanner.repository.ScanHistoryRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
